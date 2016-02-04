a756013b3308da888a3b09feb473866b378cfa7e
SUNSHINE <sarasunsh@gmail.com>
2016-02-04 11:25:55 -0500

    Simplified and commented

--- cracklepop.py
+++ cracklepop.py
@@ -1,41 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 01 13:54:37 2016
+This program will print out numbers within a given range. If the number is 
+divisible by number1 (3), the program will instead print out Crackle. If it is
+divisible by number2 (5), it will print out Pop instead of the number. If the 
+number is divisible by both number1 and number2, it will print out CracklePop.
+
+The program will default to a range of 1 through 100 (inclusive).
 
 @author: SSunshine
 """
 
 
-def crackle_pop(start, end, step=1): 
-    number1 = 3
-    number2 = 5    
-    
-    if step == 1:
-        for i in range(start, end+step):
-             #If the number is divisible by both 3 and 5, print CracklePop
-            if i % (number1 * number2) == 0:
-                print 'CracklePop'
-            #If the number is divisible by 3, print Crackle 
-            elif i % number1 == 0:
-                print "Crackle"
-            #If it's divisible by 5, print Pop. 
-            elif i % number2 == 0:
-                print "Pop"
-            else:
-                print i
+def crackle_pop(start=1, end=100): 
+    # Define the values of number1 and 2
+    number1, number2 = 3, 5  
 
-    else:    
-        i = start   
-        while i < (end + 1):
-            #If the number is divisible by both 3 and 5, print CracklePop
-            if i % (number1 * number2) == 0:
-                print 'CracklePop'
-            #If the number is divisible by 3, print Crackle 
-            elif i % number1 == 0:
-                print "Crackle"
-            #If it's divisible by 5, print Pop. 
-            elif i % number2 == 0:
-                print "Pop"
-            else:
-                print i
-            i += step
+    # Generate the inclusive range of values the program needs to evaluate        
+    for i in range(start, end+1):
+         #If the value is divisible by both numbers, print CracklePop
+        if i % (number1 * number2) == 0:
+            print 'CracklePop'
+        #If the value is divisible by number1, print Crackle 
+        elif i % number1 == 0:
+            print "Crackle"
+        #If the value is divisible by number2, print Pop. 
+        elif i % number2 == 0:
+            print "Pop"
+        # If the value is not divisible by either number, just print it 
+        else:
+            print i
